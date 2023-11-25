c0603d80e6d868279892e05902fff6befd17d6d0
Cris <cristinamitrica9@gmail.com>
2023-11-25 20:15:46 +0100

    home planets slick-carousel

--- planets/src/app/pages/home/home.component.ts
+++ planets/src/app/pages/home/home.component.ts
@@ -3,10 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
   planets: any;
 
   constructor() {}
@@ -17,22 +16,27 @@ export class HomeComponent implements OnInit {
         title: 'Mercurio',
         subtitle: 'Diario',
         image: '/assets/images/mercury.png',
-        navigateTo: "video-scan"
+        navigateTo: 'video-scan',
       },
       {
         title: 'Neptuno',
         subtitle: 'Chatbot',
         image: '/assets/images/neptune.png',
-        navigateTo: "chatbot"
+        navigateTo: 'chatbot',
       },
       {
         title: 'Marte',
         subtitle: 'Juegos',
         image: '/assets/images/mars.png',
-        navigateTo: "game"
-      }
+        navigateTo: 'game',
+      },
     ];
   }
 
-  slideConfig = { "slidesToShow": 1, "slidesToScroll": 1, "centerMode": true };
+  slideConfig = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    infinite: true,
+  };
 }
