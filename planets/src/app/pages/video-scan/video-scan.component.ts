8bd682766655fa31bf8f044a7498944cebe1e903
Jon <jonfernal@gmail.com>
2023-11-29 19:07:30 +0100

    Update video-scan

--- planets/src/app/pages/video-scan/video-scan.component.ts
+++ planets/src/app/pages/video-scan/video-scan.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { VideoScan } from 'src/app/shared/models/video-scan.model';
 import { VideoScanFetchService } from 'src/app/shared/services/fetchs/video-scan-fetch.service';
 
@@ -7,11 +9,18 @@ import { VideoScanFetchService } from 'src/app/shared/services/fetchs/video-scan
   templateUrl: './video-scan.component.html',
   styleUrls: ['./video-scan.component.css']
 })
-export class VideoScanComponent {
+
+export class VideoScanComponent implements OnInit {
+  images: Observable<any> | undefined;
 
   constructor(
     private videoScanFetchService: VideoScanFetchService,
+    private http: HttpClient,
   ) {};
+
+  ngOnInit() {
+    this.images = this.http.get('http://127.0.0.1:8000/videofeed/');
+  }
 
   uploadVideo(){
     const body: VideoScan = {
