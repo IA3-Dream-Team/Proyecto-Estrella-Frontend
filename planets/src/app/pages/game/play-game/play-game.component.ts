88acae04dee07d8c51e2f71276adee920bab60f2
Cris <cristinamitrica9@gmail.com>
2023-11-30 23:47:14 +0100

    tarea play-game mostrar juego por pantalla

--- planets/src/app/pages/game/play-game/play-game.component.ts
+++ planets/src/app/pages/game/play-game/play-game.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mini-GameSelection',
   templateUrl: './play-game.component.html',
   styleUrl: './play-game.component.css'
 })
+export class PlayGameComponent implements OnInit {
+  games: any;
 
-export class PlayGameComponent {
+  constructor() {}
+
+  ngOnInit() {
+    this.games = [
+      {
+        title: 'game1',
+        code: '<div class="container-wrapper-genially" style="position: relative; min-height: 400px; max-width: 100%;"><video class="loader-genially" autoplay="autoplay" loop="loop" playsinline="playsInline" muted="muted" style="position: absolute;top: 45%;left: 50%;transform: translate(-50%, -50%);width: 80px;height: 80px;margin-bottom: 10%"><source src="https://static.genial.ly/resources/loader-default.mp4" type="video/mp4" />Your browser does not support the video tag.</video><div id="655b4fd390cbf9001160fda4" class="genially-embed" style="margin: 0px auto; position: relative; height: auto; width: 100%;"></div></div><script>(function (d) { var js, id = "genially-embed-js", ref = d.getElementsByTagName("script")[0]; if (d.getElementById(id)) { return; } js = d.createElement("script"); js.id = id; js.async = true; js.src = "https://view.genial.ly/static/embed/embed.js"; ref.parentNode.insertBefore(js, ref); }(document));</script>',
+      },
+      // {
+      //   title: 'game2',
+      //   code: '<div class="container-wrapper-genially" style="position: relative; min-height: 400px; max-width: 100%;"><video class="loader-genially" autoplay="autoplay" loop="loop" playsinline="playsInline" muted="muted" style="position: absolute;top: 45%;left: 50%;transform: translate(-50%, -50%);width: 80px;height: 80px;margin-bottom: 10%"><source src="https://static.genial.ly/resources/loader-default.mp4" type="video/mp4" />Your browser does not support the video tag.</video><div id="655b539f90cbf90011634f4e" class="genially-embed" style="margin: 0px auto; position: relative; height: auto; width: 100%;"></div></div><script>(function (d) { var js, id = "genially-embed-js", ref = d.getElementsByTagName("script")[0]; if (d.getElementById(id)) { return; } js = d.createElement("script"); js.id = id; js.async = true; js.src = "https://view.genial.ly/static/embed/embed.js"; ref.parentNode.insertBefore(js, ref); }(document));</script>',
+      // },
+      // {
+      //   title: 'game3',
+      //   code: 'Juego 3',
+      // },
+      // {
+      //   title: 'game4',
+      //   code: 'Juego 4',
+      // },
+    ];
+  }
+
+  trackByFunction(index: number, game: any): string {
+    return game.title;
+  }
 }
-
-
